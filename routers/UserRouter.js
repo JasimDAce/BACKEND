e2ddef51833db7aa4c12c9e62e0e995b8c9d9011
JasimDAce <mohdalijasim786@gmail.com>
2024-08-01 17:37:57 +0530

    I have installed few things

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
  const Model = require('../models/UserModel');
+ const jwt = require('jsonwebtoken')
+ require('dotenv').config();
+
 
 //initialize
  const router = express.Router();
@@ -84,7 +87,41 @@ router.put('/update/:id',(req, res)=>{
         console.log(err);
         res.status(500).json(err);
     });
-})
+});
+
+router.post('/authenticate',(req,res) => {
+    Model.findOne(req.body)
+    .then((result) => {
+        if(result){
+            //JWT to generate and verify the token and .env is used
+            //payload , secretkey, expiry
+
+            const{_id, email,password} = result;
+            const payload = {_id, email,password};
+            jwt.sign(
+                payload,
+                'process.env.JWT.SECRET',
+                {expiresIn : '1hr'},
+                (err, token) => {
+                    if(err){
+                        console.log(err);
+                        res.status(500).json(err);
+                    }
+                    else{
+                        res.status(200).json({token: token});
+                    }
+                }
+            )
+
+        }
+        else{
+            res.status(401).json({message: 'Invalid Credentials'})
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
 
